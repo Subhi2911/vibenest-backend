5bec9c6ddd910221470d9af72197074d5d5b3c2c
Subhasmita Mishra <msubhasmita29@gmail.com>
2025-07-12 20:40:59 +0530

    completed backend api

--- index.js
+++ index.js
@@ -14,6 +14,8 @@ app.use(express.json());
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/blogs', require('./routes/blogs'))
 app.use('/uploads/profile', express.static('uploads/profile'));
+app.use('/uploads', express.static('uploads'));
+
 
 
 app.listen(port, () => {
--- models/Blog.js
+++ models/Blog.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const BlogSchema = new mongoose.Schema({
   author: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: 'username'
+    ref: 'user'
   },
   imageurl: {
     type: String,
